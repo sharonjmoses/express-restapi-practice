5dacf838c25177a0dd46e62d2773fb4cac35c2b8
MKS-01 <manish10mks@gmail.com>
2020-08-12 04:47:22 +0530

    modular models

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import express from 'express';
+import models from './models';
 
 const app = express();
 
@@ -11,72 +12,54 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+
   next();
 });
 
-let users = {
-  1: {
-    id: '1',
-    username: 'MKS',
-  },
-  2: {
-    id: '2',
-    username: 'DEV',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
 app.get('/session', (req, res) => {
-  return res.send(users[req.me.id]);
+  return res.send(req.context.models.users[req.context.me.id]);
 });
 
 app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(req.context.models.users));
 });
 
 app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
+  return res.send(req.context.models.users[req.params.userId]);
 });
 
 app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(req.context.models.messages));
 });
 
 app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
+  return res.send(req.context.models.messages[req.params.messageId]);
 });
 
 app.post('/messages', (req, res) => {
   const id = uuidv4();
-  // const date = Date.parse(req.body.date);
-  // const count = Number(req.body.count);
   const message = {
     id,
     text: req.body.text,
-    userId: req.me.id,
+    userId: req.context.me.id,
   };
 
-  messages[id] = message;
+  req.context.models.messages[id] = message;
 
   return res.send(message);
 });
 
 app.delete('/messages/:messageId', (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } = messages;
-  messages = otherMessages;
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
+
+  req.context.models.messages = otherMessages;
   return res.send(message);
 });
 
---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'MKS',
+  },
+  2: {
+    id: '2',
+    username: 'Dev',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Sample 1',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Sample 2',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
