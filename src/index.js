44baaafa39919a30d6a129d9fabcb843c1b21418
MKS-01 <manish10mks@gmail.com>
2020-08-12 04:35:42 +0530

    basic crud

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import 'dotenv/config';
+import { v4 as uuidv4 } from 'uuid';
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
 
 let users = {
   1: {
@@ -29,6 +39,10 @@ let messages = {
   },
 };
 
+app.get('/session', (req, res) => {
+  return res.send(users[req.me.id]);
+});
+
 app.get('/users', (req, res) => {
   return res.send(Object.values(users));
 });
@@ -45,17 +59,26 @@ app.get('/messages/:messageId', (req, res) => {
   return res.send(messages[req.params.messageId]);
 });
 
-// app.post('/users', (req, res) => {
-//   return res.send('POST HTTP method on user resource');
-// });
+app.post('/messages', (req, res) => {
+  const id = uuidv4();
+  // const date = Date.parse(req.body.date);
+  // const count = Number(req.body.count);
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
 
-// app.put('/users/:userId', (req, res) => {
-//   return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-// });
+  messages[id] = message;
 
-// app.delete('/users/:userId', (req, res) => {
-//   return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-// });
+  return res.send(message);
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const { [req.params.messageId]: message, ...otherMessages } = messages;
+  messages = otherMessages;
+  return res.send(message);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on port: ${process.env.PORT}!`);
