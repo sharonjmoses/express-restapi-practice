011768cec6fec848982a8ad6e810062e1f777244
MKS-01 <manish10mks@gmail.com>
2020-08-13 02:52:43 +0530

    mongo initial setup

--- src/models/index.js
+++ src/models/index.js
@@ -1,28 +1,42 @@
-let users = {
-  1: {
-    id: '1',
-    username: 'MKS',
-  },
-  2: {
-    id: '2',
-    username: 'Dev',
-  },
+import mongoose from 'mongoose';
+import User from './user';
+import Message from './message';
+
+const connectDb = () => {
+  return mongoose.connect(process.env.DATABASE_URL);
 };
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Sample 1',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'Sample 2',
-    userId: '2',
-  },
-};
+const models = { User, Message };
 
-export default {
-  users,
-  messages,
-};
+export { connectDb };
+
+export default models;
+
+// let users = {
+//   1: {
+//     id: '1',
+//     username: 'MKS',
+//   },
+//   2: {
+//     id: '2',
+//     username: 'Dev',
+//   },
+// };
+
+// let messages = {
+//   1: {
+//     id: '1',
+//     text: 'Sample 1',
+//     userId: '1',
+//   },
+//   2: {
+//     id: '2',
+//     text: 'Sample 2',
+//     userId: '2',
+//   },
+// };
+
+// export default {
+//   users,
+//   messages,
+// };
